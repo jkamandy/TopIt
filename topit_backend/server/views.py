10079344586df6a89710a2b58635d1d7362ee6aa
Joseph Kamandy <josephkamandy@gmail.com>
2023-12-28 19:14:17 -0800

    email sending feature working, email server up

--- topit_backend/server/views.py
+++ topit_backend/server/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.core.mail import send_mail
+from django.conf import settings
+
 
 from .models import QuoteForm
 
@@ -22,22 +25,51 @@ def submit_form(request):
             phone=data.get('phone', ''),
             comments=data.get('comments', '')
         )
-        print('created new table entry')
         new_quote.save()
-        return JsonResponse({
+
+
+        # Dont need this here, only in stand alone scripts (see test.py)
+        # settings.configure(
+        #     EMAIL_BACKEND=os.environ.get('EMAIL_BACKEND'),
+        #     EMAIL_HOST=os.environ.get('EMAIL_HOST'),
+        #     EMAIL_PORT=587,
+        #     EMAIL_USE_TLS=True,
+        #     EMAIL_HOST_USER=os.environ.get('EMAIL_HOST_USER'),
+        #     EMAIL_HOST_PASSWORD=os.environ.get('EMAIL_HOST_PASSWORD'),
+        #     RECIPIENT_ADDRESS =os.environ.get('RECIPIENT_ADDRESS'),
+        # )
+
+
+        try:
+            send_mail(
+                subject = 'TopIt Form Submission',
+                message = f'Name: {new_quote.name}\nEmail: {new_quote.email}\nPhone: {new_quote.phone}\nComments: {new_quote.comments}',
+                from_email=settings.EMAIL_HOST_USER,
+                recipient_list=[settings.RECIPIENT_ADDRESS]
+            )
+        
+            # print('sent mail')
+            return JsonResponse({
              'name': new_quote.name,
              'email': new_quote.email,
              'phone': new_quote.email,
              'comments': new_quote.comments,
              'message': 'Form submitted successfully'
              })
+        except Exception as e:
+            return JsonResponse({'error': str(e)}, status=500)
+
+    
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
 
 
 
 """
-View to return desired data 
+View to return desired data.
+url: /api/getPostedData
+
+@returns JSON serialized data
 """
 def get_posted_data(request):
     try:
--- topit_backend/topit_backend/settings.py
+++ topit_backend/topit_backend/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,3 +123,18 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+'''
+Setting up Email Server
+'''
+env = environ.Env()
+environ.Env.read_env()
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = env('EMAIL_HOST')
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = env('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = env('EMAIL_HOST_PASSWORD')
+
+RECIPIENT_ADDRESS = env('RECIPIENT_ADDRESS')
